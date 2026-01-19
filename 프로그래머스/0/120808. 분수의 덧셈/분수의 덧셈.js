41640d71de7c16ccd2cb4ae6023942df7a64d921
yammyam <96424434+yammyam@users.noreply.github.com>
2026-01-20 00:05:06 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,5 +1,5 @@
 function solution(numer1, denom1, numer2, denom2) {
-    function common(a,b){
+    function common(a,b){ //유클리드 호제법 , 최대공약수 구하는문제
         return b === 0 ? a : common(b,a%b);
     }
     let up = numer1*denom2 + numer2*denom1;
